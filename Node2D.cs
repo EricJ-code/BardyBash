7ffa3bb006f5183199749e2f61c4169bc6ac746c
10Hr <10hourmail@gmail.com>
2023-09-18 17:55:15 -0400

    test

--- Node2D.cs
+++ Node2D.cs
@@ -6,6 +6,7 @@ public partial class Node2D : Godot.Node2D
 	// Called when the node enters the scene tree for the first time.
 	public override void _Ready()
 	{
+		///dad
 	}
 
 	// Called every frame. 'delta' is the elapsed time since the previous frame.
