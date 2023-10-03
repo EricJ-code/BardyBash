92e78da8a19ebfc39e0a613c7b037e6a4d13b8b5
Nyni <nyniuni@gmail.com>
2023-10-02 20:09:03 -0400

    Committing 10/2

---
+++ Spawner.cs
@@ -0,0 +1,47 @@
+using Godot;
+using System;
+
+public partial class Spawner : Node2D
+{
+	// Called when the node enters the scene tree for the first time.
+	[Export]
+	private string playerPath = "res://Scenes/Notes/Note.tscn"; // Path to your scene
+
+	//public override void _Ready()
+	
+	//{
+		//string path = "res://Scenes/Notes/Note.tscn";
+		
+		//string path = "res://Scenes/Notes/Note.tscn";
+		//PackedScene packedScene = GD.Load<PackedScene>(path);
+		//Node2D note = packedScene.Instatiate<Node2D>();
+		//spawn(note);
+	//}
+		//var scene = ResourceLoader.Load<PackedScene>(playerPath ).Instantiate();
+		private Godot.Node note;
+
+	public override void _Ready()
+	{
+		note = ResourceLoader.Load<PackedScene>(playerPath ).Instantiate();
+		spawn();
+	}
+	// Called every frame. 'delta' is the elapsed time since the previous frame.
+
+	public override void _Process(double delta)
+	{
+
+
+	}
+	
+	/*
+	public void spawn(Godot.Node node){
+		AddChild(note);
+	}
+
+*/
+	
+	public void spawn(){
+		AddChild(note);
+	}
+
+}
