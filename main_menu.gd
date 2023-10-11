2089a955477320aa2aca09a1a3bb53663f84fcc9
CatMont <90017476+CatMont@users.noreply.github.com>
2023-10-11 16:41:08 -0400

    Options menu experiments, option scene change commented out (but still there)

--- main_menu.gd
+++ main_menu.gd
@@ -3,7 +3,8 @@ extends Control
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	#get_tree().grab()
+	pass
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
