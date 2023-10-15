32e506353bc6ec956b82f1c7da3cf8930a664d32
CatMont <90017476+CatMont@users.noreply.github.com>
2023-10-15 02:30:54 -0400

    + 3 spawners, no timing, only once

--- Scripts/gaming.gd
+++ Scripts/gaming.gd
@@ -4,6 +4,9 @@ extends Node2D
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	spawnNote("Collector1")
+	spawnNote("Collector2")
+	spawnNote("Collector3")
+	spawnNote("Collector4")
 	pass # Replace with function body.
 
 
