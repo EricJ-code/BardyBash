b854e2523b6936cf7cfcad2a64aa45cdd0657402
EricJ-code <ericjestadt@gmail.com>
2023-10-29 18:53:06 -0400

    Eric J. Estadt 10.29.23 Added 2 add-ons required for testing Added 2 "test" test scripts to display the possibilities with both the c# and GDS unit testing add-ons

--- Scripts/Stage1Base.gd
+++ Scripts/Stage1Base.gd
@@ -3,20 +3,16 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	pass
+	
+func _on_button_pressed():
 	spawnNote("Collector1")
 	spawnNote("Collector2")
 	spawnNote("Collector3")
 	spawnNote("Collector4")
-	pass # Replace with function body.
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
-	pass
 
 func spawnNote(collector):
 	get_node(collector).spawnNote()
 	
-
 
 
---
+++ addons/GDMUT/TestResult.cs
@@ -0,0 +1,72 @@
+#if TOOLS
+using Godot;
+using System.Collections.Generic;
+
+namespace GdMUT.Components;
+
+[Tool]
+public partial class TestResult : Control
+{
+    [Export]
+    private RichTextLabel _typeName;
+
+    [Export]
+    private VBoxContainer _methodList;
+
+    private List<(MethodResult, TestFunction)> _functions = new();
+    private const string TYPE_NAME_FORMAT = "[b][font_size=24][center]{0}[/center][/font_size][/b]";
+    private string _typeNameStr;
+
+    public override void _EnterTree()
+    {
+        base._EnterTree();
+        foreach (Node child in _methodList.GetChildren())
+        {
+            child.QueueFree();
+        }
+        _functions.Clear();
+    }
+
+    public void SetTypeName(string typeName)
+    {
+        _typeNameStr = typeName;
+        _typeName.Text = string.Format(TYPE_NAME_FORMAT, typeName);
+    }
+
+    public void UpdateResult()
+    {
+        int numSuccess = 0;
+        foreach (var (methodResult, function) in _functions)
+        {
+            methodResult.Update();
+            numSuccess += function.Result.IsSuccess ? 1 : 0;
+        }
+
+        _typeName.Text = string.Format(
+            TYPE_NAME_FORMAT,
+            _typeNameStr + $" ({numSuccess}/{_functions.Count})"
+        );
+        if (numSuccess == _functions.Count)
+        {
+            _typeName.Modulate = new Color(0, 1, 0);
+        }
+        else if (numSuccess == 0)
+        {
+            _typeName.Modulate = new Color(1, 0, 0);
+        }
+        else
+        {
+            _typeName.Modulate = new Color(1, 0.9f, 0);
+        }
+    }
+
+    public void AddMethodResult(TestFunction function)
+    {
+        var methodResultScene = GD.Load<PackedScene>("res://addons/GDMUT/MethodResult.tscn");
+        var methodResult = methodResultScene.Instantiate<MethodResult>();
+        methodResult.SetMethodResult(function);
+        _methodList.AddChild(methodResult);
+        _functions.Add((methodResult, function));
+    }
+}
+#endif
