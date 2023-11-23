78dd81eae91f6945015d0cc867658d5dd7d3bfdd
snasar1 <sumiranasar1@gmail.com>
2023-11-23 14:53:17 -0500

    Updated GameManagement

---
+++ GameManagement/Exmaple_ Menu.gd
@@ -0,0 +1,26 @@
+extends Control
+
+func _on_play_pressed():
+	pass
+
+
+
+func _on_options_pressed():
+	pass
+
+
+func _on_Quit_pressed():
+	pass
+
+
+
+
+
+# Called when the node enters the scene tree for the first time.
+#func _ready():
+#	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+#func _process(delta):
+	#pass
