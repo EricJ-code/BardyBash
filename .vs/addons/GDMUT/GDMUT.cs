56178ad66b06f3c12a5275bc308890faeccdbc10
snasar1 <sumiranasar1@gmail.com>
2023-12-09 17:36:48 -0500

    Added comments to the code

---
+++ .vs/addons/GDMUT/GDMUT.cs
@@ -0,0 +1,26 @@
+#if TOOLS
+using Godot;
+
+namespace GdMUT;
+
+[Tool]
+public partial class GDMUT : EditorPlugin
+{
+	private Control _dock;
+
+	public override void _EnterTree()
+	{
+		base._EnterTree();
+		_dock = GD.Load<PackedScene>("res://addons/GDMUT/Dock.tscn").Instantiate<Control>();
+		AddControlToDock(DockSlot.RightUl, _dock);
+		GD.Print("Successfully loaded GDMUT");
+	}
+
+	public override void _ExitTree()
+	{
+		base._ExitTree();
+		RemoveControlFromDocks(_dock);
+		_dock?.Free();
+	}
+}
+#endif
